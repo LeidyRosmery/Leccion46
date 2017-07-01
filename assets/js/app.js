ec593b6d80a30ea000a053974f43012134077daf
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-30 19:38:53 -0500

    ultimo master

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,22 @@ var lastScrollTop = 0;
 
 $( window ).scroll(function() {
    console.log("entra");
+  var pantalla= $(window).width();
   var currentScroll=window.pageYOffset || document.documentElement.scrollTop;
-  if (currentScroll > lastScrollTop) {
-     menu.css("display","block");
-     console.log("hacia abajo");
-  }
-  else{
-    if( currentScroll <= 3 ){
-      menu.css("display","none");
+  if (pantalla>360) {
+
+    if (currentScroll > lastScrollTop) {
+       menu.css("display","block");
+       console.log("hacia abajo");
+       var pantalla= $(window).attr('width');
+       console.log(pantalla);
+    }
+    else{
+      if( currentScroll <= 3 ){
+        menu.css("display","none");
+      }
     }
   }
+
     lastScrollTop = currentScroll;
 });
